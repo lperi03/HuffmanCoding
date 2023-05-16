47ffcd55e30e5a832a3dbd79049b0512ce415cda
lperi03 <lperi@berkeley.edu>
2023-05-16 05:12:47 -0700

    made some progress
    added functions to calculate symbol frequencies using frequency table

--- HuffmanCompression/HuffmanCompression.cpp
+++ HuffmanCompression/HuffmanCompression.cpp
@@ -4,34 +4,75 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
 
 // Structure representing node in Huffman tree
 struct HuffmanNode {
     // defining character/symbol and the frequency associated with the symbol
+    //also checking if given node is a leaf with boolean
     char symbol;
     int frequency;
+    bool isLeaf;
     
     // Pointers to left and right child Huffman nodes
     HuffmanNode* leftChild;
     HuffmanNode* rightChild;
 
-    HuffmanNode(char symbol, int frequency)
-        : symbol(symbol), frequency(frequency), leftChild(nullptr), rightChild(nullptr) {} 
+    HuffmanNode(char symbol, int frequency, bool isLeaf)
+        : symbol(symbol), frequency(frequency), isLeaf(isLeaf), leftChild(nullptr), rightChild(nullptr) {} 
 };
 
 // Structure representing Huffman tree, with pointer to base/root node
 
 struct HuffmanTree {
     HuffmanNode* base;
+    
+    string originalFileName;
+    long long originalFileSize;
+    int uniqueSymbols;
+    int totalSymbols;
+    double compressionRatio;
+    double entropy;
 
     HuffmanTree()
-        : base(nullptr) {}
+        : base(nullptr), originalFileName(" "), originalFileSize(0), uniqueSymbols(0), totalSymbols(0), compressionRatio(0.0), entropy(0.0) {}
 };
 
-int main()
-{
-    std::cout << "Hello World!\n";
+unordered_map<char, int> calcSymbolFrequency(const string& fileName) {
+    unordered_map<char, int> frequencyTable;
+    ifstream inputFile(fileName);
+
+    //need to add some contingency to handle errors with input file
+    if (!inputFile) {
+        cerr << "Error opening input file! : " << fileName << endl;
+        return frequencyTable;
+    }
+
+    char character;
+    while (inputFile.get(character)) {
+        frequencyTable[character]++;
+    }
+
+    inputFile.close();
+    return frequencyTable;
 }
+
+
+HuffmanNode* buildHuffmanTree(const unordered_map<char, int>& frequencyTable) {
+    struct nodeComparisons {
+
+    };
+}
+
+
+
+//int main()
+//{
+    //cout << "Hello World!\n";
+//}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
